50afb29b8f055ebbd678414c902b87d084afb83a
rajibscode <rajibsarder321@gmail.com>
2022-12-19 12:59:34 +0600

    my personal portfolios

---
+++ portfolio/assets/js/main.js
@@ -0,0 +1,13 @@
+
+// slick slider
+$(document).ready(function(){
+
+
+    $('.slider').slick({
+        arrows:false,
+        dots:true,
+        appendDots:'.slider-dots',
+        dotsClass:'dots',
+        
+    });
+});
